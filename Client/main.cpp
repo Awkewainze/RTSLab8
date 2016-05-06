1cb7edd1e9fdcd3b3da0ef89214385435dad26ab
Dylan Walseth <dylanwalseth@gmail.com>
2016-05-06 14:48:02 -0500

    Info

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
 using namespace std;
+// Receive sound from microphone (or file)
+// Calculate volume
+// Take video capture
+// Send to server
 
+// run with 'avclient <audio capture device> <destination hostname> <destination audio port> <destination video port> <length of time to send in seconds>'
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
--- Server/main.cpp
+++ Server/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
 using namespace std;
+// Computer
+// Display video
+// On Qt gui
+// Play sound
+// From network
 
+// run with 'avserver <audio playback hardware> <audio port> <video port>'
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
